dd2f5fcc7160c85de97b3ac783d12d3d11c1ca9f
Diego Pirineus Patti <diegopatti.dev@gmail.com>
2023-02-01 12:20:09 -0300

    3
    Main project complete.

--- 4-main_project/index.js
+++ 4-main_project/index.js
@@ -1,7 +1,72 @@
 // Assignment 4 - Rock, Paper or Scissors Game
 
-const computerPlay = Math.floor(Math.random() * 3);
+const choices = [`rock`, `paper`, `scissors`];
 
+function computerPlay(){
+  const compChoice = choices[
+    Math.floor(Math.random() * choices.length)];
+  return compChoice;
+}
 
+function check(playerSelection, computerSelection){
+  if(playerSelection == computerSelection){
+    return `Tie`
+  }else if(
+    (playerSelection == `rock` && computerSelection == `scissors`) ||
+    (playerSelection == `paper` && computerSelection == `rock`) ||
+    (playerSelection == `scissors` && computerSelection == `paper`)
+  ){
+    return `Player`;
+  }else{
+    return `Computer`;
+  }
+}
 
-console.log(computerPlay)
+function playRound(playerSelection, computerSelection){
+  const result = check(playerSelection, computerSelection)
+  if(result == `Tie`){
+    return `The round ended in a tie.`
+  } else if(result == `Player`){
+    return `You Won! ${playerSelection} beats ${computerSelection}.`
+  }else{
+    return `You Lose! ${computerSelection} beats ${playerSelection}.`
+  }
+}
+
+function playerChoice(){
+  let validChoice = false;
+  while(validChoice == false){
+    const playerChoice = prompt(`Please, choose rock, paper or scissors`);
+    if(playerChoice == null){
+      continue;
+    }
+    const lowerCaseChoice = playerChoice.toLowerCase();
+    if(choices.includes(lowerCaseChoice)){
+      validChoice = true;
+      return lowerCaseChoice
+    }
+  }
+}
+
+function game(){
+  let playerScore = 0;
+  let computerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = playerChoice();
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection))
+    if(check(playerSelection, computerSelection) == `Player`){
+      playerScore++
+    }else if (check(playerSelection, computerSelection) == `Computer`){
+      computerScore++
+    }
+  }
+  console.log(`___________________________________`)
+  if(playerScore > computerScore){
+    console.log(`Game over. Player was the winner! Congratulations!`);
+  } else if(playerScore < computerScore){
+    console.log(`Game over. Computer was the winner!`);
+  } else {
+    console.log(`Nobody won. The game was a tie.`);
+  }
+}
